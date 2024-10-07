8b093b7dcc20515cfaa893be908d414675a778ec
ahmed <mail.ahmed6@gmail.com>
2024-10-07 21:03:33 +1100

    Merging data into Database.

--- src/main/java/kidneyproject/merge/CleanFiles.java
+++ src/main/java/kidneyproject/merge/CleanFiles.java
@@ -1,5 +1,5 @@
 
-package org.example;
+package kidneyproject.merge;
 
 import java.io.*;
 import java.sql.*;
--- src/main/java/kidneyproject/merge/MergeFiles.java
+++ src/main/java/kidneyproject/merge/MergeFiles.java
@@ -1,4 +1,4 @@
-package org.example;
+package kidneyproject.merge;
 
 import java.io.*;
 import java.sql.*;
